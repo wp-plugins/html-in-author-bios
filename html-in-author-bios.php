39cd062d3e460eaf53b662b9f6024b0a6ee9403d
pankajanupam <pankajanupam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-05 06:57:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/html-in-author-bios/trunk@500552 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ html-in-author-bios.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: html in author bios
+Plugin URI: http://pankajanupam.in/wordpress-plugins/
+Description: html in autor bios
+Version: 1.0
+Author: PANKAJ ANUPAM
+Author URI: http://pankajanupam.in
+
+* LICENSE
+    Copyright 2011 PANKAJ ANUPAM  (email : mymail.anupam@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
---
+++ readme.txt
@@ -0,0 +1,18 @@
+=== html in author bios ===
+Contributors: pankajanupam
+Tags: subdomains, categories, subdomain
+Requires at least: 2.8
+Tested up to: 3.3.1
+
+
+== Description ==
+
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+None
+
+== Changelog ==
